0f9c3d08e68d56a9bbf6311c6440e20f20439720
Robin <171968246+metalbeard7@users.noreply.github.com>
2025-12-22 19:30:39 -0800

    Add operator functions and variables

---
+++ script.js
@@ -0,0 +1,18 @@
+let a;
+let b;
+let operator;
+
+// add function
+let add = (a, b) => a + b;
+
+// subtract function
+let subtract = (a, b) => a - b;
+
+// multiply function
+let multiply = (a, b) => a * b;
+
+// divide function
+let divide = (a, b) => a / b;
+
+// operate function
+const operate = (a, b, operator) => operator(a, b);
