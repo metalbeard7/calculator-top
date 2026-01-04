9b1d5597f3041cb14a27d6280d10d1beb65a461c
Robin <171968246+metalbeard7@users.noreply.github.com>
2026-01-03 18:02:47 -0800

    Add function for displaying numbers

--- script.js
+++ script.js
@@ -16,3 +16,18 @@ let divide = (a, b) => a / b;
 
 // operate function
 const operate = (a, b, operator) => operator(a, b);
+let result = operate(a, b, operator);
+
+
+// display function
+const numberBtns = document.querySelectorAll('.number');
+const operatorBtns = document.querySelectorAll('.operator');
+const calDisplay = document.getElementById('display');
+
+
+numberBtns.forEach((button) => {
+    button.addEventListener('click', () => {
+        calDisplay.textContent += button.textContent;
+    });
+});
+
